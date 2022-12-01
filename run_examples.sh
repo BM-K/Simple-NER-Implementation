7ed7318b95cc45c08cb64847f05f30bc7b642db5
BM-K <klbm126@gmail.com>
2022-12-01 14:43:41 +0900

    Update run_examples.sh

--- run_examples.sh
+++ run_examples.sh
@@ -1,7 +1,5 @@
-#CUDA_VISIBEL_DEVICES=0 python main.py --train True --test False --batch_size 32 --lr 0.00005 --path_to_save ./output/ --ckpt bert_32_5e05.pt
-CUDA_VISIBEL_DEVICES=0 python main.py --train False --test True  --path_to_save ./output/ --ckpt bert_32_5e05.pt
+CUDA_VISIBEL_DEVICES=0 python main.py --train True --test False --model klue/bert-base --batch_size 32 --lr 0.00005 --path_to_save ./output/ --ckpt bert_32_5e05.pt
+CUDA_VISIBEL_DEVICES=0 python main.py --train False --test True  --model klue/bert-base --path_to_save ./output/ --ckpt bert_32_5e05.pt
 
 CUDA_VISIBEL_DEVICES=0 python main.py --train True --test False --model klue/roberta-base --batch_size 32 --lr 0.00005 --path_to_save ./output/ --ckpt roberta_32_5e05.pt
-
-CUDA_VISIBEL_DEVICES=0 python main.py --train False --test True  --path_to_save ./output/ --ckpt roberta_32_5e05.pt
-
+CUDA_VISIBEL_DEVICES=0 python main.py --train False --test True  --model klue/roberta-base --path_to_save ./output/ --ckpt roberta_32_5e05.pt
